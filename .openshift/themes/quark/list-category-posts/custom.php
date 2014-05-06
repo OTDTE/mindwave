0f83db49de83766a87fe14eabb96706efac83e24
OTDTE <info@otdte.com>
2014-05-06 14:57:17 -0500

    lcp

--- .openshift/themes/quark/list-category-posts/custom.php
+++ .openshift/themes/quark/list-category-posts/custom.php
@@ -1,48 +1,101 @@
 <?php
 /*
-Plugin Info & license stuff...
+Plugin Name: List Category Posts - Template "Default"
+Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
+Description: Template file for List Category Post Plugin for Wordpress which is used by plugin by argument template=value.php
+Version: 0.9
+Author: Radek Uldrych & Fernando Briano 
+Author URI: http://picandocodigo.net http://radoviny.net
 */
-$lcp_output = '';    
-//Show category?
-    if ($atts['catlink'] == 'yes'){
-        $cat_link = get_category_link($lcp_category_id);
-        $cat_title = get_cat_name($lcp_category_id);
-        $lcp_output = '<div class="topic-heading"><a href="' . $cat_link . '" title="' . $cat_title . '">' . $cat_title . '</a></div>';
-    }
-$lcp_output .= '<div class="post">';//For default ul
 
-//Posts loop:
+/* Copyright 2009  Radek Uldrych  (email : verex@centrum.cz), Fernando Briano (http://picandocodigo.net)
 
-foreach($catposts as $single):
-    $lcp_output .= '<h2 class="entry-title"><a href="' . get_permalink($single->ID) . '">' . $single->post_title . '</a></h2>';
-    //Show comments?
-    if($atts['comments'] == yes){
-        $lcp_output .= ' (' . $single->comment_count . ')';
-    }
-    //Style for date:
-    if($atts['date']=='yes'){
-        $lcp_output .= ' <div class="entry-meta"> ' . get_the_time($atts['dateformat'], $single) . '</div>';
-    }
-    //Show author?
-    if($atts['author']=='yes'){
-        $lcp_userdata = get_userdata($single->post_author);
-        $lcp_output .=' <div class="entry-meta">' .$lcp_userdata->display_name . '</div>';
-    }
-    //Show thumbnail?
-    if($atts['thumbnail']=='yes'){
-        $lcp_output .= '<div class="lcp_thumbnail"><a href="' . get_permalink($single->ID) . '">' . get_the_post_thumbnail($single->ID, array('40','40')) .'</a></div>';
-    }
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or 
+any later version.
 
-    //Show content?
-    if($atts['content']=='yes' && $single->post_content){
-        $lcpcontent = apply_filters('the_content', $single->post_content); // added to parse shortcodes
-        $lcpcontent = str_replace(']]>', ']]&gt', $lcpcontent); // added to parse shortcodes
-        $lcp_output .= '<p>' . $lcpcontent . '</p>'; // line tweaked to output filtered content
-    }
-    //Show excerpt?
-    if($atts['excerpt']=='yes' && !($atts['content']=='yes' && $single->post_content) ){
-        $lcp_output .= lcp_excerpt($single);
-    }
-    endforeach;
-$lcp_output .= '</div>';
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * The format for templates changed since version 0.17.
+ * Since this code is included inside CatListDisplayer, $this refers to
+ * the instance of CatListDisplayer that called this file.
+ */
+
+/* This is the string which will gather all the information.*/
+$lcp_display_output = '';
+
+// Show category link:
+$lcp_display_output .= $this->get_category_link('strong');
+
+//Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
+$lcp_display_output .= '<ul class="lcp_catlist">';
+
+/**
+ * Posts loop.
+ * The code here will be executed for every post in the category.
+ * As you can see, the different options are being called from functions on the
+ * $this variable which is a CatListDisplayer.
+ *
+ * The CatListDisplayer has a function for each field we want to show.
+ * So you'll see get_excerpt, get_thumbnail, etc.
+ * You can now pass an html tag as a parameter. This tag will sorround the info
+ * you want to display. You can also assign a specific CSS class to each field.
+ */
+foreach ($this->catlist->get_categories_posts() as $single){
+  //Start a List Item for each post:
+  $lcp_display_output .= "<li>";
+
+  //Show the title and link to the post:
+  $lcp_display_output .= $this->get_post_title($single);
+
+  //Show comments:
+  $lcp_display_output .= $this->get_comments($single);
+
+  //Show date:
+  $lcp_display_output .= ' ' . $this->get_date($single);
+
+  //Show author
+  $lcp_display_output .= $this->get_author($single);
+
+  //Custom fields:
+  $lcp_display_output .= $this->get_custom_fields($this->params['customfield_display'], $single->ID);
+
+  //Post Thumbnail
+  $lcp_display_output .= $this->get_thumbnail($single);
+
+  /**
+   * Post content - Example of how to use tag and class parameters:
+   * This will produce:<p class="lcp_content">The content</p>
+   */
+  $lcp_display_output .= $this->get_content($single, 'p', 'lcp_content');
+
+  /**
+   * Post content - Example of how to use tag and class parameters:
+   * This will produce:<div class="lcp_excerpt">The content</div>
+   */
+  $lcp_display_output .= $this->get_excerpt($single, 'div', 'lcp_excerpt');
+
+  //Close li tag
+  $lcp_display_output .= '</li>';
+}
+
+$lcp_display_output .= '</ul>';
+
+// If there's a "more link", show it:
+$lcp_display_output .= $this->catlist->get_morelink();
+
+//Pagination
+$lcp_display_output .= $this->get_pagination();
+
+$this->lcp_output = $lcp_display_output;
 ?>
